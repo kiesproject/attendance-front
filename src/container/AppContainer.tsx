8d0dde9dfadb99d0feb69e0c9a8fbb66e31434b1
nnsnico5214 <yuji.jampan.blue@gmail.com>
2018-04-14 23:01:37 +0900

    Change index.html and Add Initial react compoents

---
+++ src/container/AppContainer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class AppContainer extends React.Component<any, any> {
+  render() {
+    return (
+      <h1>Welcome to サボらん</h1>
+    );
+  }
+}
+
+export default AppContainer;
