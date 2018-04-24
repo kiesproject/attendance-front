6893519cbdd9275e644b0681697a753be44b8c05
nnsnico <yuji.jampan.blue@gmail.com>
2018-04-24 23:04:24 +0900

    Add Input forms

--- src/components/screens/Login.tsx
+++ src/components/screens/Login.tsx
@@ -2,7 +2,28 @@ import * as React from 'react';
 
 class Login extends React.Component<any, any> {
   render() {
-    return <div>Please Login</div>;
+    return (
+      <div className="loginComponent">
+        <h3 className="header">Please Login</h3>
+        <div className="form">
+          <dl>
+            <dt>
+              <label>ID</label>
+            </dt>
+            <dd>
+              <input type="text" name="name" className="input" />
+            </dd>
+            <dt>
+              <label>Password</label>
+            </dt>
+            <dd>
+              <input type="text" name="name" className="input" />
+            </dd>
+          </dl>
+          <button className="loginButton">ログインor登録</button>
+        </div>
+      </div>
+    );
   }
 }
 
