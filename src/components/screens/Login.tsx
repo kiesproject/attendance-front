7b3c9d0ac2b25fdbd945b9eef9c52c1c810234cc
ayatk <git@ayatk.com>
2018-05-06 17:58:38 +0900

    Improvement Login form design

--- src/components/screens/Login.tsx
+++ src/components/screens/Login.tsx
@@ -3,24 +3,26 @@ import * as React from 'react';
 class Login extends React.Component<any, any> {
   render() {
     return (
-      <div className="loginComponent">
-        <h3 className="header">Please Login</h3>
-        <div className="form">
-          <dl>
-            <dt>
-              <label>ID</label>
-            </dt>
-            <dd>
-              <input type="text" name="name" className="input" />
-            </dd>
-            <dt>
-              <label>Password</label>
-            </dt>
-            <dd>
-              <input type="text" name="name" className="input" />
-            </dd>
-          </dl>
-          <button className="loginButton">ログインor登録</button>
+      <div className="siimple-form login-form">
+        <div className="siimple-form-title">ログイン</div>
+        <div className="siimple-form-field">
+          <div className="siimple-form-field-label">ユーザー名</div>
+          <input
+            type="text"
+            className="siimple-input siimple-input--fluid"
+            placeholder="kiesproject"
+          />
+        </div>
+        <div className="siimple-form-field">
+          <div className="siimple-form-field-label">パスワード</div>
+          <input
+            type="password"
+            className="siimple-input siimple-input--fluid"
+            placeholder="*********"
+          />
+        </div>
+        <div className="siimple-form-field">
+          <div className="siimple-btn siimple-btn--blue">ログイン</div>
         </div>
       </div>
     );
