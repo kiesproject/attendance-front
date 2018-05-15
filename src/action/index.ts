6bdf700dc578533ff9593fe5bb8ffda3e790851d
nnsnico <yuji.jampan.blue@gmail.com>
2018-05-15 12:03:41 +0900

    Add login action and reducer for account

--- src/action/index.ts
+++ src/action/index.ts
@@ -13,7 +13,14 @@ interface Decrease {
 const LOGIN = 'LOGIN';
 export interface Login {
   type: string;
-  id: string;
+  username: string;
+  isAdmin: boolean;
+}
+
+const LOGIN_ASYNC = 'LOGIN_ASYNC';
+export interface LoginAsync {
+  type: string;
+  username: string;
   password: string;
 }
 
@@ -31,10 +38,18 @@ export function decrease(n: number): Decrease {
   };
 }
 
-export function login(id: string, password: string): Login {
+export function login(username: string, isAdmin: boolean): Login {
   return {
     type: LOGIN,
-    id,
+    username,
+    isAdmin,
+  };
+}
+
+export function loginAsync(username: string, password: string): LoginAsync {
+  return {
+    type: LOGIN_ASYNC,
+    username,
     password,
   };
 }
