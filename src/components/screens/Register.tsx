6d0e5ad780bbc36bd54df4e1a393c12691bc4646
nnsnico <yuji.jampan.blue@gmail.com>
2018-05-28 17:45:27 +0900

    Add handling input error

--- src/components/screens/Register.tsx
+++ src/components/screens/Register.tsx
@@ -6,12 +6,13 @@ import Error from '../../models/Error';
 interface RegisterProperties {
   account: Account;
   error: Error;
-  refreshError(): void;
+  handleError(isError: boolean, message: string): void;
 }
 
 interface RegisterState {
   name: string;
   password: string;
+  passwordOnce: string;
 }
 
 class Register extends React.Component<RegisterProperties, RegisterState> {
@@ -20,21 +21,34 @@ class Register extends React.Component<RegisterProperties, RegisterState> {
     this.state = {
       name: '',
       password: '',
+      passwordOnce: '',
     };
     this.handleRegister = this.handleRegister.bind(this);
   }
 
   componentWillMount() {
-    const { refreshError } = this.props;
-    refreshError();
+    const { handleError } = this.props;
+    handleError(false, '');
   }
 
   handleRegister(
     event: React.FormEvent<HTMLElement>,
     name: string,
     password: string,
+    passwordOnce: string,
   ) {
-    console.log('handleRegister', name, password);
+    const { handleError } = this.props;
+    if (name === '' || password === '') {
+      return;
+    }
+    if (password !== passwordOnce) {
+      event.preventDefault();
+      return handleError(true, '異なるパスワードを入力しています');
+    }
+    handleError(false, '');
+    // TODO: dispatch register
+
+    event.preventDefault();
   }
 
   render() {
@@ -49,7 +63,6 @@ class Register extends React.Component<RegisterProperties, RegisterState> {
           <input
             type="text"
             className="siimple-input siimple-input--fluid"
-            placeholder="kiesproject"
             required
             value={this.state.name}
             onChange={event => this.setState({ name: event.target.value })}
@@ -64,13 +77,33 @@ class Register extends React.Component<RegisterProperties, RegisterState> {
             value={this.state.password}
             onChange={event => this.setState({ password: event.target.value })}
           />
+          <div className="siimple-field-helper">
+            英数字と記号のみ使用可能です
+          </div>
+          <input
+            type="password"
+            className="siimple-input siimple-input--fluid"
+            required
+            value={this.state.passwordOnce}
+            onChange={event =>
+              this.setState({ passwordOnce: event.target.value })
+            }
+          />
+          <div className="siimple-field-helper">
+            もう一度パスワードを入力してください。
+          </div>
         </div>
         <div className="siimple-form-field">
           <button
-            className="siimple-btn siimple-btn--blue"
+            className="siimple-btn siimple-btn--green"
             type="submit"
             onClick={event =>
-              this.handleRegister(event, this.state.name, this.state.password)
+              this.handleRegister(
+                event,
+                this.state.name,
+                this.state.password,
+                this.state.passwordOnce,
+              )
             }
           >
             登録
