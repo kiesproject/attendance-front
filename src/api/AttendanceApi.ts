3dc624476663e2b783f9ae22003ab15d6a68fc68
nnsnico <yuji.jampan.blue@gmail.com>
2018-05-28 18:51:25 +0900

    Add postRegister

--- src/api/AttendanceApi.ts
+++ src/api/AttendanceApi.ts
@@ -3,7 +3,24 @@ const headers = {
   'Content-Type': 'application/json',
 };
 
-// TODO: put postResister
+export function postRegister(name: string, password: string) {
+  return fetch(`${endPoint}/register`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      name,
+      password,
+    }),
+    mode: 'cors',
+  }).then(res => {
+    if (!res.ok) {
+      return res.json().then(data => {
+        throw new Error(data.message);
+      });
+    }
+    return res.json();
+  });
+}
 
 export function postLogin(name: string, password: string) {
   return fetch(`${endPoint}/login`, {
