826efc90ca097462600062c08cc2ef910f657b6f
nnsnico <yuji.jampan.blue@gmail.com>
2018-05-28 18:52:10 +0900

    Add to call register api by saga

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,13 +1,14 @@
 import { all, call, put, takeEvery } from 'redux-saga/effects';
 
 import { error as errorAction, login, LoginAsync } from '../action/';
-import { postLogin } from '../api/AttendanceApi';
+import { postLogin, postRegister } from '../api/AttendanceApi';
 
 function* loginAsync(action: LoginAsync) {
   const { username: name, password } = action;
   try {
     const response = yield call(postLogin, name, password);
     // TODO: get administer authority by response
+    // TODO: レスポンスは今後変わるかも？
     yield put(login(response.user, true));
   } catch (error) {
     yield put(errorAction(true, error.message));
@@ -16,7 +17,13 @@ function* loginAsync(action: LoginAsync) {
 
 function* registerAsync(action: any) {
   const { username: name, password } = action;
-  yield console.log('registerAsync', name, password);
+  try {
+    yield call(postRegister, name, password);
+    // TODO: レスポンスは今後変わるかも？
+    yield put(login(name, false));
+  } catch (error) {
+    yield put(errorAction(true, error.message));
+  }
 }
 
 function* watchLogin() {
